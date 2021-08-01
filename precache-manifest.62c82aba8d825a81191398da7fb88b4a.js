3f2a6e0f3efc89433095b417035e8c0db5222778
Matias Cardone <zero.xm7@gmail.com>
2021-08-01 12:49:28 -0300

    Updates

---
+++ precache-manifest.62c82aba8d825a81191398da7fb88b4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d261cb864b272901187a2ac88c6112a",
+    "url": "/platzi-badges/index.html"
+  },
+  {
+    "revision": "7ecd334e502fc0980235",
+    "url": "/platzi-badges/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "810356cf068f2d87bdf7",
+    "url": "/platzi-badges/static/css/main.49e41303.chunk.css"
+  },
+  {
+    "revision": "7ecd334e502fc0980235",
+    "url": "/platzi-badges/static/js/2.906ed157.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/platzi-badges/static/js/2.906ed157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810356cf068f2d87bdf7",
+    "url": "/platzi-badges/static/js/main.507517b8.chunk.js"
+  },
+  {
+    "revision": "c1f4ffb2d0ae472a3560",
+    "url": "/platzi-badges/static/js/runtime-main.2a82efe4.js"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/platzi-badges/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/platzi-badges/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/platzi-badges/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/platzi-badges/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platzi-badges/static/media/stars.0abcdfcd.svg"
+  }
+]);
